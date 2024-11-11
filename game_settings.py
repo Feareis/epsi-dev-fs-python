68327882875fe49c1def52227014858f7a2df50e
Feareis <v.reisse@protonmail.com>
2024-11-11 15:28:22 +0100

    Primary Color
    ajout des couleurs primaire dans game_settings

--- game_settings.py
+++ game_settings.py
@@ -1,18 +1,35 @@
 """
-Couleurs
+Color
 """
+WHITE = (255, 255, 255)          # Blanc
+BLACK = (0, 0, 0)                # Noir
+RED = (255, 0, 0)                # Rouge
+GREEN = (0, 255, 0)              # Vert
+BLUE = (0, 0, 255)               # Bleu
+YELLOW = (255, 255, 0)           # Jaune
+ORANGE = (255, 165, 0)           # Orange
+PURPLE = (128, 0, 128)           # Violet
+PINK = (255, 192, 203)           # Rose
+BROWN = (139, 69, 19)            # Marron
+GRAY = (128, 128, 128)           # Gris
+DARK_GRAY = (64, 64, 64)         # Gris foncé
+LIGHT_GRAY = (192, 192, 192)     # Gris clair
+GOLD = (255, 215, 0)             # Or
 
-COULEUR_CASE_VIDE = (255, 255, 255)  # Blanc
-COULEUR_CASE_INDESTRUCTIBLE = (128, 128, 128)  # Gris
-COULEUR_BRIQUE_CASSABLE = (160, 82, 45)  # Marron
-COULEUR_JOUEUR = (0, 128, 0)  # Vert
-COULEUR_ENNEMI = (255, 0, 0)  # Rouge
-COULEUR_FOND = (255, 255, 255)  # Blanc pour le fond
-COULEUR_BOMB = (255, 215, 0)  # Jaune
+
+
+COULEUR_CASE_VIDE = WHITE
+COULEUR_CASE_INDESTRUCTIBLE = GRAY
+COULEUR_BRIQUE_CASSABLE = BROWN
+COULEUR_JOUEUR = GREEN
+COULEUR_ENNEMI = RED
+COULEUR_FOND = WHITE
+COULEUR_BOMB = YELLOW
+
 
 
 """
-Taille des cases
+Case Size
 """
 
 TAILLE_CASE = 40  # Taille des cases du plateau
