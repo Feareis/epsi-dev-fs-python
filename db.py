fab609e605ebdb1594043aaa4691bbb2ced7d765
Feareis <v.reisse@protonmail.com>
2024-11-12 06:25:15 +0100

    update struct + db

---
+++ db.py
@@ -0,0 +1,63 @@
+import sqlite3
+import os
+
+SCORE_DB_PATH = os.path.join("db", "scores.db")
+GAME_DB_PATH = os.path.join("db", "game.db")
+
+def initialize_scores_db():
+    conn = sqlite3.connect(SCORE_DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS high_scores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            player_name TEXT NOT NULL,
+            score INTEGER NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+def initialize_game_db():
+    conn = sqlite3.connect(GAME_DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS game_save (
+            id INTEGER PRIMARY KEY,
+            player_position TEXT,
+            foes_positions TEXT,
+            score INTEGER,
+            game_plate TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+def save_score(player_name, score):
+    conn = sqlite3.connect("db/scores.db")  # DB Name
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO high_scores (player_name, score) VALUES (?, ?)
+        ''', (player_name, score))
+    conn.commit()
+    conn.close()
+
+
+def game_end(score):
+    player_name = input("Entrez votre nom : ")
+    save_score(player_name, score)  # Sauvegarde du score du joueur
+
+
+def get_top_scores(limit=5):
+    conn = sqlite3.connect("db/scores.db")  # DB Name
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT player_name, score
+        FROM high_scores 
+        ORDER BY score 
+        DESC LIMIT ?
+        ''', (limit,))
+    scores = cursor.fetchall()
+    conn.close()
+    return scores
