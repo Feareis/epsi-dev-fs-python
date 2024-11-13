b745e09ae5750da1059f0792a9b9efb14993e9ed
Feareis <v.reisse@protonmail.com>
2024-11-13 05:26:28 +0100

    Update db.py
    ?? charger et restaurer les game à 2 joeurs ??

--- db.py
+++ db.py
@@ -104,3 +104,71 @@ def load_game():
         game_plate = eval(result[5])
         return player_position, foes_positions, score, nb_line, nb_column, game_plate
     return None
+
+
+# db game_2p ???
+"""
+def initialize_game_2p_db():
+    conn = sqlite3.connect(GAME_DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS game_save (
+            id INTEGER PRIMARY KEY, 
+            player1_position TEXT, 
+            player2_position TEXT, 
+            foes_positions TEXT, 
+            score_p1 INTEGER, 
+            score_p2 INTEGER, 
+            nb_line INTEGER, 
+            nb_column INTEGER, 
+            game_plate TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+def save_game_2p(player1_position, player2_position, foes_positions, score_p1, score_p2, nb_line, nb_column, game_plate):
+    conn = sqlite3.connect(GAME_DB_PATH)
+    cursor = conn.cursor()
+    # Supprimer la sauvegarde précédente
+    cursor.execute('DELETE FROM game_save')
+    cursor.execute('''
+        INSERT INTO game_save (id, player1_position, player2_position, foes_positions, score_p1, score_p2, nb_line, nb_column, game_plate)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (
+            1,
+            str(player1_position),
+            str(player2_position),
+            str(foes_positions),
+            score_p1,
+            score_p2,
+            nb_line,
+            nb_column,
+            str(game_plate)
+        ))
+    conn.commit()
+    conn.close()
+
+def load_game_2p():
+    conn = sqlite3.connect(GAME_DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT player1_position, player2_position, foes_positions, score_p1, score_p2, nb_line, nb_column, game_plate
+        FROM game_save 
+        WHERE id = 1
+    ''')
+    result = cursor.fetchone()
+    conn.close()
+    if result:
+        player1_position = eval(result[0])
+        player2_position = eval(result[1])
+        foes_positions = eval(result[2])
+        score_p1 = result[3]
+        score_p2 = result[4]
+        nb_line = result[5]
+        nb_column = result[6]
+        game_plate = eval(result[7])
+        return player1_position, player2_position, foes_positions, score_p1, score_p2, nb_line, nb_column, game_plate
+    return None
+"""
