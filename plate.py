bf19ae3ff328e1651c4fd623b3c46237a4aec64a
Feareis <v.reisse@protonmail.com>
2024-11-13 06:02:41 +0100

    Update plate.py
    Ajout de la gestion plateau pour 2 joueur

--- plate.py
+++ plate.py
@@ -42,7 +42,7 @@ def random_plate(nb_line, nb_column, iratio=None, dratio=None):
         plate.append(row)
 
     # Assurer une zone vide autour de la position initiale du joueur
-    x, y = gs.STARTING_PLAYER_POSITION
+    x, y = gs.STARTING_PLAYER1_POSITION
     if 0 <= x < nb_line and 0 <= y < nb_column:
         plate[x][y] = " "  # Position initiale du joueur
         if y + 1 < nb_column:
@@ -51,6 +51,49 @@ def random_plate(nb_line, nb_column, iratio=None, dratio=None):
             plate[x + 1][y] = " "
         if x + 1 < nb_line and y + 1 < nb_column:
             plate[x + 1][y + 1] = " "
+
+    return plate
+
+
+def random_plate_2p(nb_line, nb_column, iratio=None, dratio=None):
+    # Ratios aléatoires pour les murs indestructibles et cassables si non fournis
+    iratio = iratio or random.uniform(0, 0.2)
+    dratio = dratio or random.uniform(0, 0.4)
+
+    plate = []
+    for i in range(nb_line):
+        row = []
+        for j in range(nb_column):
+            rand_value = random.random()
+            if rand_value < iratio:
+                row.append("X")  # Mur indestructible
+            elif rand_value < iratio + dratio:
+                row.append("B")  # Brique cassable
+            else:
+                row.append(" ")  # Case vide
+        plate.append(row)
+
+    # Assurer une zone vide autour de la position initiale du joueur
+    x, y = gs.STARTING_PLAYER1_POSITION
+    x2, y2 = gs.STARTING_PLAYER2_POSITION
+    if 0 <= x < nb_line and 0 <= y < nb_column:
+        plate[x][y] = " "  # Position initiale du joueur 1
+        # Zone joueur 1
+        if y + 1 < nb_column:
+            plate[x][y + 1] = " "
+        if x + 1 < nb_line:
+            plate[x + 1][y] = " "
+        if x + 1 < nb_line and y + 1 < nb_column:
+            plate[x + 1][y + 1] = " "
+    elif 0 <= x2 < nb_line and 0 <= y2 < nb_column:
+        plate[x2][y2] = " "  # Position initiale du joueur 2
+        # Zone joueur 2
+        if y2 + 1 < nb_column:
+            plate[x2][y2 + 1] = " "
+        if x2 + 1 < nb_line:
+            plate[x2 + 1][y2] = " "
+        if x2 + 1 < nb_line and y2 + 1 < nb_column:
+            plate[x2 + 1][y2 + 1] = " "
 
     return plate
 
@@ -64,7 +107,39 @@ def view_plate(screen, plate, player_position, foes):
 
             # Vérifie si la position actuelle correspond à celle du joueur
             if (i, j) == player_position:
-                pygame.draw.rect(screen, gs.COULEUR_JOUEUR, rect)
+                pygame.draw.rect(screen, gs.COULEUR_JOUEUR1, rect)
+
+            # Vérifie si la position actuelle correspond à celle d'un ennemi
+            elif (i, j) in foes:
+                pygame.draw.rect(screen, gs.COULEUR_ENNEMI, rect)
+
+            # Vérifie si la case actuelle est un mur indestructible
+            elif case == "X":
+                pygame.draw.rect(screen, gs.COULEUR_CASE_INDESTRUCTIBLE, rect)
+
+            # Vérifie si la case actuelle est une brique cassable
+            elif case == "B":
+                pygame.draw.rect(screen, gs.COULEUR_BRIQUE_CASSABLE, rect)
+
+            # Affiche une case vide si elle ne contient ni joueur, ni ennemi, ni mur, ni brique cassable
+            else:
+                pygame.draw.rect(screen, gs.COULEUR_CASE_VIDE, rect)
+
+
+def view_plate_2p(screen, plate, player1_position, player2_position, foes):
+    for i, row in enumerate(plate):  # Parcourt chaque ligne du plateau (i -> index de la ligne, row -> contenu de la ligne)
+        for j, case in enumerate(row):  # Parcourt chaque case de la ligne (j -> index de la case, case -> contenu de la case)
+
+            # Défini le rectangle pour la case actuelle
+            rect = j * gs.TAILLE_CASE, i * gs.TAILLE_CASE, gs.TAILLE_CASE, gs.TAILLE_CASE
+
+            # Vérifie si la position actuelle correspond à celle du joueur
+            if (i, j) == player1_position:
+                pygame.draw.rect(screen, gs.COULEUR_JOUEUR1, rect)
+
+            # Vérifie si la position actuelle correspond à celle du joueur
+            if (i, j) == player2_position:
+                pygame.draw.rect(screen, gs.COULEUR_JOUEUR2, rect)
 
             # Vérifie si la position actuelle correspond à celle d'un ennemi
             elif (i, j) in foes:
