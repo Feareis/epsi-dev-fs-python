774f7f9c3c06297c72e8328fc6ce0cd0588f867f
Feareis <v.reisse@protonmail.com>
2024-11-13 03:09:10 +0100

    add comment

--- bomb.py
+++ bomb.py
@@ -34,15 +34,15 @@ def update_bombs(screen, game_plate, foes, player_position, nb_line, nb_column):
 
 def explode_bomb(position, game_plate, foes, player_position, nb_line, nb_column):
     x, y = position  # Coordonnées de la bombe
-    explosion_area = [(x, y), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]  # zone d'explosion : la position de la bombe et les cases adjacentes
+    explosion_area = [(x, y), (x + 2, y), (x - 2, y), (x, y + 2), (x, y - 2)]  # zone d'explosion : la position de la bombe et les 2 cases adjacentes
     player_hit = False
     for ex, ey in explosion_area:  # Parcourt chaque case dans la zone d'explosion
         if 0 <= ex < nb_line and 0 <= ey < nb_column:  # Vérifie si les coordonnées sont dans les limites du plateau
             if (ex, ey) == player_position:  # Vérifie si le joueur est dans la zone d'explosion
                 player_hit = True  # Indique que le joueur a été touché par l'explosion
-            if game_plate[ex][ey] == "B":  # Détruit une brique cassable dans la zone d'explosion
+            if game_plate[ex][ey] == "B":  # Vérifie si une brique cassable est dans la zone d'explosion
                 game_plate[ex][ey] = " "  # Remplace la brique cassable par une case vide
-            elif (ex, ey) in foes:  # Supprime un ennemi touché par l'explosion
+            elif (ex, ey) in foes:  # Vérifie si un ennemi ou plusieurs ennemis est dans la zone d'explosion
                 foes.remove((ex, ey))  # Retire l'ennemi de la liste des ennemis
 
     return player_hit  # Retourne True si le joueur est touché par l'explosion
